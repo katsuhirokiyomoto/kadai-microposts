de701ab962fecb37f8be927595ab1456467b002e
katsuhirokiyomoto <katsuhiro.k@hotmail.com>
2017-06-05 17:49:22 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,17 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
